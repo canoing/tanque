5d0de7de7d7c35dde31385d1d621b534bea0d36d
cano_ing <josebulac960@gmail.com>
2024-01-03 14:59:38 -0500

    rapido xd

--- autornil.lua
+++ autornil.lua
@@ -6,7 +6,12 @@ function Raton:new(x, y)
     self = setmetatable({},Raton)
     self.x = x
     self.y = y 
+    self.vel = 100
     return self
+end
+function Raton:update(dt)
+    self.y = self.y + (self.vel* dt)
+    
 end
 function Raton:draw()
     love.graphics.draw( Enemigo,self.x , self.y)
--- main.lua
+++ main.lua
@@ -12,13 +12,17 @@ function love.update(dt)
 
     if Tiempo == 0 then
         local x = math.random(1, 600)
-        local y = 0
+        local y = -10
         table.insert(Rt, Raton:new(x, y))
       Tiempo = 30
        else
         Tiempo = Tiempo - 1
     end
+    for i, v in ipairs(Rt) do
+        v:update(dt)
+    end
     Tanque:update(dt)
+    
 
 end
 function love.draw()
